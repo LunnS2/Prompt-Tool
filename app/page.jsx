d20499c7ae486ba33649cb0c8b95713225a79c68
Lunn <thafm.work@gmail.com>
2024-08-17 19:56:05 +0100

    First Commit

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient"> AI-powered Prompts </span>
+      </h1>
+      <p className="desc text-center">
+        PrompTool is an open source AI promting tool for modern world to discover, create and share creative prompts
+      </p>
+      {/* Feed */}
+    </section>
+  )
+}
+
+export default Home
